1c050ed30a49e75a796e08586df08bf3a11a7911
Mario Fernandez <diddleslip@gmail.com>
2024-01-15 18:11:30 -0500

    Princeton algs4.jar file added

--- courseIntro/RandomWord.java
+++ courseIntro/RandomWord.java
@@ -1,5 +1,10 @@
-package courseIntro;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
 
 public class RandomWord {
-  
+  public static void main(String[] args) {
+
 }
+
