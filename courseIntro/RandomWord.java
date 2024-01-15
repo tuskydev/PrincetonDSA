8d52ce355954fdf622757e13218147ad1071f758
Mario Fernandez <diddleslip@gmail.com>
2024-01-15 18:41:14 -0500

    Finished RandomWord problem

--- courseIntro/RandomWord.java
+++ courseIntro/RandomWord.java
@@ -5,6 +5,17 @@ import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomWord {
   public static void main(String[] args) {
+    String champion = "";
+    int i = 1;
 
+    while (!StdIn.isEmpty()) {
+      String currentWord = StdIn.readString();
+      if (StdRandom.bernoulli(1.0 / i)) {
+        champion = currentWord;
+      }
+      i++;
+    }
+    StdOut.println(champion);
+  }
 }
 
