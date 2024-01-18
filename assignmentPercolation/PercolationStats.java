f54b392af6a6d0146be6495c150f00afe1225b87
Mario Fernandez <diddleslip@gmail.com>
2024-01-18 16:56:32 -0500

    Added needed methods

--- assignmentPercolation/PercolationStats.java
+++ assignmentPercolation/PercolationStats.java
@@ -4,7 +4,34 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 
 public class PercolationStats {
-  public static void main(String[] args) {
-    System.out.println("Hello SECOND world!");
+
+  // perform independent trials on an n-by-n grid
+  public PercolationStats(int n, int trials) {
+
   }
+
+  // sample mean of percolation threshold
+  public double mean() {
+
+  }
+
+  // sample standard deviation of percolation threshold
+  public double stddev() {
+
+  }
+
+  // low endpoint of 95% confidence interval
+  public double confidenceLo() {
+
+  }
+
+  // high endpoint of 95% confidence interval
+  public double confidenceHi() {
+
+  }
+
+ // test client (see below)
+ public static void main(String[] args) {
+
+ }
 }
