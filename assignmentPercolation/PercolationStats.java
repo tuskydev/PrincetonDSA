c28c2e4a7148b3946ee81efcb08cc9e8c56a7f49
Mario Fernandez <diddleslip@gmail.com>
2024-01-18 12:26:09 -0500

    Setup algs4 into .java files and ready to begin assessment

--- assignmentPercolation/PercolationStats.java
+++ assignmentPercolation/PercolationStats.java
@@ -1,5 +1,10 @@
-package assignmentPercolation;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 
 public class PercolationStats {
-
+  public static void main(String[] args) {
+    System.out.println("Hello SECOND world!");
+  }
 }
