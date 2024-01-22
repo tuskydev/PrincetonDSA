f24a6cb07d4b37fc84b5aa3da79c40744a0c3225
Mario Fernandez <diddleslip@gmail.com>
2024-01-22 13:14:42 -0500

    Implemented Percolation method, that initiates a 3d grid of n x n size and has flow,value, and root values for each cell

--- assignmentPercolation/Percolation.java
+++ assignmentPercolation/Percolation.java
@@ -6,7 +6,7 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 
 public class Percolation {
-  private int[][] grid;
+  private Object[][][] grid;
 
   // creates n-by-n grid, with all sites initially blocked
   public Percolation(int n) {
@@ -14,30 +14,30 @@ public class Percolation {
       throw new IllegalArgumentException("Grid size must be greater than 0");
     }
 
-    grid = new int[n][n];
+    grid = new Object[n][n][3];
+    int counter = 0;
 
     for (int i = 0; i < n; i++) {
       for (int e = 0; e < n; e++) {
-        grid[i][e] = e + 1;
-      }
-    }
-
-    for (int i = 0; i < grid.length; i++) {
-      for (int e = 0; e < grid.length; e++) {
-        System.out.println(grid[i][e]);
+        grid[i][e][0] = false;       // Flow is set false
+        grid[i][e][1] = counter;     // Value of cell
+        grid[i][e][2] = counter;     // Root value (starts rooted to itself)
+        counter++;
       }
     }
   }
 
-  // // opens the site (row, col) if it is not open already
-  // public void open(int row, int col) {
-  //   if (row > grid[0].length) {
-  //     throw new IllegalArgumentException("ROW outside prescribed range");
-  //   }
-  //   if (col > grid[0].length) {
-  //     throw new IllegalArgumentException("COL outside prescribed range");
-  //   }
-  // }
+  // opens the site (row, col) if it is not open already
+  public void open(int row, int col) {
+    if (row > grid[0].length) {
+      throw new IllegalArgumentException("ROW outside prescribed range");
+    }
+    if (col > grid[0].length) {
+      throw new IllegalArgumentException("COL outside prescribed range");
+    }
+
+
+  }
 
   // // is the site (row, col) open?
   // public boolean isOpen(int row, int col) {
