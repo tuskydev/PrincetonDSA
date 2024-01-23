0025a23f49bb5e16527302c4467d4c52e2ca8105
Mario Fernandez <diddleslip@gmail.com>
2024-01-23 05:53:29 -0500

    Created checkForError and xyTo1D private methods for repeated used!

--- assignmentPercolation/Percolation.java
+++ assignmentPercolation/Percolation.java
@@ -10,16 +10,25 @@ public class Percolation {
   private int size;
   private WeightedQuickUnionUF qf;
 
-  private void checkForError(int n) {
-    if (n <= grid.length) {
-      throw new IllegalArgumentException();
+  private void checkForError(int n, boolean beginning) {
+    if (beginning && n <= 0) {
+      throw new IllegalArgumentException("Grid size must be greater than 0.");
+    } else if (!beginning && n > size || !beginning && n <= 0) {
+      throw new IllegalArgumentException("Row and Columns input too low/high!");
     }
+  }
+
+  private int xyTo1D(int x, int y) {      // We can swap int to a String!
+    return Integer.parseInt(x +  "" + y);
   }
 
   // creates n-by-n grid, with all sites initially blocked
   public Percolation(int n) {
+    checkForError(n, true);
+
     size = n;
     qf = new WeightedQuickUnionUF(size * size * 2);
+    qf.Union();
   }
 
   // // opens the site (row, col) if it is not open already
