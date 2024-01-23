f5c0acd0eea745a1af48ecd6c369edaea0eef81a
Mario Fernandez <diddleslip@gmail.com>
2024-01-23 07:34:46 -0500

    Reformatted xyTo1D method and implemented multiple helper methods

--- assignmentPercolation/Percolation.java
+++ assignmentPercolation/Percolation.java
@@ -6,11 +6,14 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 
 public class Percolation {
-  private int[][] grid;
-  private int size;
+  private final int size;
+  private final int top;
+  private final int bottom;
+  private int openSites;
+  private boolean[][] opened;
   private WeightedQuickUnionUF qf;
 
-  private void checkForError(int n, boolean beginning) {
+  private void checkNumForErrors(int n, boolean beginning) {
     if (beginning && n <= 0) {
       throw new IllegalArgumentException("Grid size must be greater than 0.");
     } else if (!beginning && n > size || !beginning && n <= 0) {
@@ -18,33 +21,48 @@ public class Percolation {
     }
   }
 
-  private int xyTo1D(int x, int y) {      // We can swap int to a String!
-    return Integer.parseInt(x +  "" + y);
+  private int xyTo1D(int x, int y) {
+    return size * x + y + 1;
   }
 
   // creates n-by-n grid, with all sites initially blocked
   public Percolation(int n) {
-    checkForError(n, true);
+    checkNumForErrors(n, true);
 
     size = n;
-    qf = new WeightedQuickUnionUF(size * size * 2);
-    qf.Union();
+    top = 0;
+    bottom = size * size + 1;
+    openSites = 0;
+    opened = new boolean[size][size];
+    qf = new WeightedQuickUnionUF(size * size + 2);
+
+    System.out.println(qf.count());
   }
 
-  // // opens the site (row, col) if it is not open already
-  // public void open(int row, int col) {
+  // opens the site (row, col) if it is not open already
+  public void open(int row, int col) {
+    checkNumForErrors(row--, false);
+    checkNumForErrors(col--, false);
 
-  // }
+    opened[row][col] = true;
+  }
 
-  // // is the site (row, col) open?
-  // public boolean isOpen(int row, int col) {
+  // is the site (row, col) open?
+  public boolean isOpen(int row, int col) {
+    checkNumForErrors(row--, false);
+    checkNumForErrors(col--, false);
 
-  // }
+    if (opened[row][col]) {
+      return true;
+    } else{
+      return false;
+    }
+  }
 
-  // // is the site (row, col) full?
-  // public boolean isFull(int row, int col) {
-
-  // }
+  // is the site (row, col) full?
+  public boolean isFull(int row, int col) {
+    qf.find();
+  }
 
   // // returns the number of open sites
   // public int numberOfOpenSites() {
